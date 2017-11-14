7eb7e99c49b8c89412496a56b8bd734a91e85021
estertambunan <32484899+estertambunan@users.noreply.github.com>
2017-11-14 17:24:16 +0700

    Add files via upload

---
+++ PBO11_10116466_Latihan1/src/model/Latihan1UTS.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author user
+ */
+public class Latihan1UTS {
+    String nim,nama;
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    public void modelMahasiswa(String nim,String nama){
+        this.nim=nim;
+        this.nama=nama;
+        
+        
+    }
+    
+    
+}
+
+   
---
+++ PBO11_10116466_Latihan1/src/pbo11_10116466_latihan1/PBO11_10116466_Latihan1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11_10116466_latihan1;
+
+/**
+ *
+ * @author user
+ */
+public class PBO11_10116466_Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
